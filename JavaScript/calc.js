e6f58155d0113e2416b8487d2521d6d5740ae844
Irzercks47 <irzadiarifin@gmail.com>
2022-02-26 14:18:17 +0700

    adding some more to calc,js and adding storage.js

--- JavaScript/calc.js
+++ JavaScript/calc.js
@@ -55,10 +55,19 @@ function performCalculation() {
         result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber)
     }
  
+       // objek yang akan dikirimkan sebagai argumen fungsi putHistory()
+    const history = {
+        firstNumber: calculator.firstNumber,
+        secondNumber: calculator.displayNumber,
+        operator: calculator.operator,
+        result: result
+    }
+    putHistory(history);
     calculator.displayNumber = result;
+    renderHistory();
 }
- 
- 
+
+
 const buttons = document.querySelectorAll(".button");
 for (let button of buttons) {
     button.addEventListener('click', function (event) {
