c77f39783970f984089136fdf15f562de786bd91
Irzercks47 <irzadiarifin@gmail.com>
2022-03-16 14:18:52 +0700

    modifying rubik

--- JavaScript/rubik.js
+++ JavaScript/rubik.js
@@ -1,7 +1,10 @@
 const gambar = document.getElementById('gambar')
 let playButt = document.querySelectorAll('button')[3]
+let links = document.getElementById("links")
 
 gambar.setAttribute('width', '400')
 gambar.setAttribute('height', '215')
 
 playButt.setAttribute("disabled","disabled")
+let linkInner = links.innerHTML
+let linkText = links.innerText
