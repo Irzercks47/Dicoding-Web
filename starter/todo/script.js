39c7d6a8c295357015bdf5474371e2c0e7ce09c5
Irzercks47 <irzadiarifin@gmail.com>
2022-03-18 13:22:52 +0700

    modify script adn index in todo

--- starter/todo/script.js
+++ starter/todo/script.js
@@ -36,13 +36,19 @@ function generateTodoObject(id, task, timestamp, isCompleted) {
 }
 
 document.addEventListener(RENDER_EVENT, function () {
-    // console.log(todos);
     const uncompletedTODOList = document.getElementById("todos");
     uncompletedTODOList.innerHTML = "";
     
+    const completedTODOList = document.getElementById("completed-todos");
+    completedTODOList.innerHTML = "";
+    
     for(todoItem of todos){
         const todoElement = makeTodo(todoItem);
-        uncompletedTODOList.append(todoElement);
+    
+        if(todoItem.isCompleted == false)
+            uncompletedTODOList.append(todoElement);
+        else
+            completedTODOList.append(todoElement);
     }
 });
 
@@ -68,9 +74,17 @@ function makeTodo(todoObject) {
         undoButton.addEventListener("click", function () {
             undoTaskFromCompleted(todoObject.id);
         });
+
+        undoButton.addEventListener("click", function () {
+            undoTaskFromCompleted(todoObject.id);
+        });
     
         const trashButton = document.createElement("button");
         trashButton.classList.add("trash-button");
+        trashButton.addEventListener("click", function () {
+            removeTaskFromCompleted(todoObject.id);
+        });
+
         trashButton.addEventListener("click", function () {
             removeTaskFromCompleted(todoObject.id);
         });
@@ -91,3 +105,45 @@ function makeTodo(todoObject) {
     return container;
 }
 
+function addTaskToCompleted(todoId) {
+    const todoTarget = findTodo(todoId);
+    if(todoTarget == null) return;
+
+    todoTarget.isCompleted = true;
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function findTodo(todoId){
+    for(todoItem of todos){
+        if(todoItem.id === todoId){
+            return todoItem
+        }
+    }
+    return null
+}
+
+function removeTaskFromCompleted(todoId) {
+    const todoTarget = findTodoIndex(todoId);
+    if(todoTarget === -1) return;
+    todos.splice(todoTarget, 1);
+
+    document.dispatchEvent(new Event(RENDER_EVENT));
+} 
+
+function undoTaskFromCompleted(todoId){
+    const todoTarget = findTodo(todoId);
+    if(todoTarget == null) return;
+    
+    
+    todoTarget.isCompleted = false;
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function findTodoIndex(todoId) {
+    for(index in todos){
+        if(todos[index].id === todoId){
+            return index
+        }
+    }
+    return -1
+}
