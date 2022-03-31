7a394dcaffb1edf2e188c2547b0a51cea74d0eea
Irzercks47 <irzadiarifin@gmail.com>
2022-03-31 16:11:00 +0700

    adding  things

--- js fundamental/obj.js
+++ js fundamental/obj.js
@@ -46,3 +46,38 @@ const car = new Car("H121S", "Honda", 4);
 // car.startEngines();
 // car.droveOff()
 car.info()
+
+class Vehicle {
+    constructor(licensePlate, manufacture) {
+        this.licensePlate = licensePlate;
+        this.manufacture = manufacture;
+        this.engineActive = false;
+    }
+    /*
+    kode lainnya
+    */
+}
+
+/* kode lainnya dalam pembuatan class Car, 
+Motorcycle, dsb. */
+
+class VehicleFactory {
+    static repair(vehicles) {
+    vehicles.forEach(vehicle => {
+        console.log(`Kendaraan ${vehicle.licensePlate} sedang melakukan perawatan`)
+    })
+    }
+}
+
+const johnCar = new Car("H121S", "Honda", 4);
+const tomMotor = new Motorcycle("GF121J", "Yamaha", 2);
+const dimasCar = new Car("TA1408K", "Tesla", 4);
+
+/* Pemanggilan method repair langsung dari class-nya */
+VehicleFactory.repair([johnCar, tomMotor, dimasCar]);
+
+/* output:
+Kendaraan H121S sedang melakukan perawatan
+Kendaraan GF121J sedang melakukan perawatan
+Kendaraan TA1408K sedang melakukan perawatan
+*/
