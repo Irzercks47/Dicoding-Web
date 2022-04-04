a31b0080cf06e64a8cdaa1c4be6761523afd3c3a
Irzercks47 <irzadiarifin@gmail.com>
2022-04-04 09:53:24 +0700

    adding custElement

---
+++ js fundamental/custElement.js
@@ -0,0 +1,7 @@
+//cara membuat custom element
+//jadi image-figure ini akan berfungsi seperti div karena tidak punya fungsi khusus
+class ImageFigure extends HTMLElement {
+
+}
+
+customElements.define("image-figure", ImageFigure);
