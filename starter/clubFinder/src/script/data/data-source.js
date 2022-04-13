65544861af1197d2c1b8e309dd066ec0d4096943
Irzercks47 <irzadiarifin@gmail.com>
2022-04-13 11:39:04 +0700

    adding fetch to clubFinder

--- starter/clubFinder/src/script/data/data-source.js
+++ starter/clubFinder/src/script/data/data-source.js
@@ -1,19 +1,32 @@
-import clubs from './clubs.js';
+// import clubs from './clubs.js';
 class DataSource{
-    static searchClub(keyword) {
-        return new Promise((res, reject) => {
+    // static searchClub(keyword) {
+    //     return new Promise((res, reject) => {
 
-            const filteredClubs = clubs.filter((club) => {
-                return club.name.toUpperCase().includes(keyword.toUpperCase());
-            });
+    //         const filteredClubs = clubs.filter((club) => {
+    //             return club.name.toUpperCase().includes(keyword.toUpperCase());
+    //         });
         
-            if (filteredClubs.length) {
-                res(filteredClubs);
-            } else {
-                reject(`${keyword} is not found`);
+    //         if (filteredClubs.length) {
+    //             res(filteredClubs);
+    //         } else {
+    //             reject(`${keyword} is not found`);
+    //         }
+    //     })
+    // };
+    static searchClub(keyword) {
+        return fetch(`https://sports-api.dicoding.dev/teams/search?t=${keyword}`)
+        .then(resp => {
+            return resp.json()
+        })
+        .then(data => {
+            if(data.teams){
+                return Promise.resolve(data.teams)
+            }else{
+                return Promise.reject(`{keyword} is not found`)
             }
         })
-    };
+    }
 }
 
 export default DataSource
