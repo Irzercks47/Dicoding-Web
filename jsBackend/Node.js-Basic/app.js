e4b9ae6c038b2f233cf64f32afbdbd629b8488c6
Irzercks47 <irzadiarifin@gmail.com>
2022-04-27 12:37:26 +0700

    adding modularization and process

--- jsBackend/Node.js-Basic/app.js
+++ jsBackend/Node.js-Basic/app.js
@@ -1,4 +1,31 @@
-const server = new Server({
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : 'dicoding.com',
-});
+// const cpuInformation = process.memoryUsage();
+// //taking argument in command
+// const firstName = process.argv[2];
+// const lastName = process.argv[3];
 
+// console.log(`Hello ${firstName} ${lastName}`);
+
+// console.log(cpuInformation);
+
+// console.log(process.argv)
+
+// /* output
+// Hello firstname lastname
+// {
+//     rss: 14569472,
+//     heapTotal: 2654208,
+//     heapUsed: 1788896,
+//     external: 855681,
+//     arrayBuffers: 9898
+// }
+// */
+
+const coffee = require('./coffee')
+
+console.log(coffee)
+
+const { firstName, lastName } = require('./user');
+
+
+console.log(firstName);
+console.log(lastName);
