047808b3eab9138522634152fd580d4f8f2eca66
Irzercks47 <irzadiarifin@gmail.com>
2022-04-28 11:55:50 +0700

    adding more to routes and its explanation

--- jsBackend/HAPI-WEB-SERVER/routes.js
+++ jsBackend/HAPI-WEB-SERVER/routes.js
@@ -36,12 +36,32 @@ const routes = [
     },
     //path parameter
     //bila mempunayi entity yang banyak kita bisa menggunakan path parameter
+    //bila tidak ditambah dengan "?" bila kita akses users maka akan eror untuk menghindari ini maka menggunakan
+    //path optional yaitu dengan menggunakan "?"
+    //kita tidak bisa menggunakannya seperti ini /{one?}/{two} karena optional path hanya bisa pada akhir param
     {
         method: 'GET',
-        path: '/users/{username}',
+        path: '/users/{username?}',
         handler: (request, h) => {
-            const { username } = request.params;
+            const { username = 'Stranger' } = request.params;
             return `Hello, ${username}!`;
+        },
+    },
+    //query params 
+    //metode lain dalam pengambilan data pada hapi adalah query params yang mengambil data localhost:5000?name=harry&location=bali
+    //pada contoh diatas terdapat 2 param untuk mengambilnya kita bisa menggunakan query params
+    {
+        method: 'GET',
+        path: '/hello/{name?}',
+        handler: (request, h) => {
+            const { name = 'Stranger' } = request.params;
+            const { lang } = request.query
+
+            if(lang == 'id'){
+                return `hai, selamat pogi ${name}`
+            }
+
+            return `Hello, ${name}!`;
         },
     },
 ];
