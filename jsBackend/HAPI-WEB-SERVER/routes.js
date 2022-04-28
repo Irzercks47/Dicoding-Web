8e220025c251fe56b1dbefde667bbd0de4a901af
Irzercks47 <irzadiarifin@gmail.com>
2022-04-28 11:32:15 +0700

    initial commit

---
+++ jsBackend/HAPI-WEB-SERVER/routes.js
@@ -0,0 +1,49 @@
+const routes = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Homepage';
+        },
+    },
+    {
+        method: '*',
+        path: '/',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/about',
+        handler: (request, h) => {
+            return 'About page';
+        },
+    },
+    {
+        method: '*',
+        path: '/about',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method';
+        },
+    },
+    {
+        method: '*',
+        path: '/{any*}',
+        handler: (request, h) => {
+            return 'Halaman tidak ditemukan';
+        },
+    },
+    //path parameter
+    //bila mempunayi entity yang banyak kita bisa menggunakan path parameter
+    {
+        method: 'GET',
+        path: '/users/{username}',
+        handler: (request, h) => {
+            const { username } = request.params;
+            return `Hello, ${username}!`;
+        },
+    },
+];
+
+module.exports = routes;
