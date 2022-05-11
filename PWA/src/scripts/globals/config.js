854bdc2808e89c3446bddc4d4f290a433782272d
Irzercks47 <irzadiarifin@gmail.com>
2022-05-11 17:11:54 +0700

    adding manifest, service worker, and cache

--- PWA/src/scripts/globals/config.js
+++ PWA/src/scripts/globals/config.js
@@ -1,8 +1,9 @@
 const CONFIG = {
-    KEY: 'YOUR_API_KEY',
+    KEY: '81aee0e2d8863809f46bb3ca772a3780',
     BASE_URL: 'https://api.themoviedb.org/3/',
     BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
     DEFAULT_LANGUAGE: 'en-us',
+    CACHE_NAME: new Date().toISOString(),
 };
 
 export default CONFIG;
---
+++ PWA/src/scripts/sw.js
@@ -0,0 +1,23 @@
+import 'regenerator-runtime';
+import CacheHelper from './utils/cache-helper';
+const { assets } = global.serviceWorkerOption;
+
+self.addEventListener('install', (event) => {
+    console.log('Installing Service Worker ...');
+    self.skipWaiting();
+
+    event.waitUntil(CacheHelper.cachingAppShell([...assets, './']));
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Activating Service Worker ...');
+
+    event.waitUntil(CacheHelper.deleteOldCache());
+});
+
+self.addEventListener('fetch', (event) => {
+    // console.log(event.request);
+
+    // event.respondWith(fetch(event.request));
+    event.respondWith(CacheHelper.revalidateCache(event.request));
+});
