40418a41ca30bcae550a2884b57c72d6986af150
Irzercks47 <irzadiarifin@gmail.com>
2022-05-11 17:18:17 +0700

    adding more

--- PWA/src/scripts/globals/config.js
+++ PWA/src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    KEY: '81aee0e2d8863809f46bb3ca772a3780',
+    KEY: 'YOUR_API_KEY',
     BASE_URL: 'https://api.themoviedb.org/3/',
     BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
     DEFAULT_LANGUAGE: 'en-us',
